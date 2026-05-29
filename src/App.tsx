6f8f8299e348b9d09dc40b49aa0141b1b53ad2d8
insightfulBiscuit <93067476+insightfulBiscuit@users.noreply.github.com>
2026-05-29 15:34:07 -0700

    fix: bug with jsx extension

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
